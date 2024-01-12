4a1ec0accd721c5dae00a1c0d59cc86a542b5b72
Bruno Cerqueira <brmcerqueira@gmail.com>
2024-01-12 17:21:31 -0300

    Adjust

--- config.ts
+++ config.ts
@@ -5,7 +5,7 @@ export const config = {
     campaignId: Deno.env.has("CAMPAIGN_ID") ? parseInt(Deno.env.get("CAMPAIGN_ID")!) : 0,
     discordToken: Deno.env.get("DISCORD_TOKEN"),
     storytellerId: Deno.env.get("STORYTELLER_ID"), 
-    host: Deno.env.get("HOST"),
+    host: Deno.env.get("BOT_HOST"),
     port: Deno.env.has("PORT") ? parseInt(Deno.env.get("PORT")!) : 3000,
     level: (Deno.env.get("LOG_LEVEL") || "DEBUG") as LevelName
 };
