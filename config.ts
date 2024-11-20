814306704724f80b2e3480febe2008d745d74bd2
Bruno Cerqueira <brmcerqueira@gmail.com>
2024-11-20 13:44:18 -0300

    Adjust

--- config.ts
+++ config.ts
@@ -1,7 +1,6 @@
 import { LevelName } from "./deps.ts";
 
 export const config = {
-    campaignId: Deno.env.has("CAMPAIGN_ID") ? parseInt(Deno.env.get("CAMPAIGN_ID")!) : 0,
     discordToken: Deno.env.get("DISCORD_TOKEN"),
     storytellerId: Deno.env.get("STORYTELLER_ID"), 
     host: Deno.env.get("BOT_HOST"),
