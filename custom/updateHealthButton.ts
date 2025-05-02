21cdc0459e204093279de60a9373138d874734b2
Bruno <brmcerqueira@gmail.com>
2025-05-02 14:42:53 -0300

    Adjust

--- custom/updateHealthButton.ts
+++ custom/updateHealthButton.ts
@@ -1,13 +1,21 @@
 import { damageParse } from "../commands/damage.ts";
 import { ButtonComponent } from "../deps.ts";
 import { buildCharacterUpdateSolver } from "../solver/buildCharacterUpdateSolver.ts";
-import { ButtonOptions, DamageInput, button } from "./common.ts";
+import { button, ButtonOptions, DamageInput } from "./common.ts";
 
-export const updateHealthButton: (options: ButtonOptions, value: number, aggravated: boolean) => ButtonComponent = button<DamageInput>(
+export const updateHealthButton: (
+  options: ButtonOptions,
+  value: number,
+  aggravated: boolean,
+) => ButtonComponent = button<DamageInput>(
   (context) => {
     const value = parseInt(context[0]);
     const aggravated = "true" == context[1];
-    return { superficial: aggravated ? undefined : value, aggravated: aggravated ? value: undefined, add: true }
+    return {
+      superficial: aggravated ? undefined : value,
+      aggravated: aggravated ? value : undefined,
+      add: true,
+    };
   },
-  buildCharacterUpdateSolver(damageParse((c) => c.health), false)
+  buildCharacterUpdateSolver(damageParse((c) => c.health), true),
 );
--- custom/updateHungerButton.ts
+++ custom/updateHungerButton.ts
@@ -1,9 +1,12 @@
 import { parseField } from "../commands/module.ts";
 import { ButtonComponent } from "../deps.ts";
 import { buildCharacterUpdateSolver } from "../solver/buildCharacterUpdateSolver.ts";
-import { ButtonOptions, button } from "./common.ts";
+import { button, ButtonOptions } from "./common.ts";
 
-export const updateHungerButton: (options: ButtonOptions, value: number) => ButtonComponent = button(
+export const updateHungerButton: (
+  options: ButtonOptions,
+  value: number,
+) => ButtonComponent = button(
   (context) => parseInt(context[0]),
-  buildCharacterUpdateSolver(parseField<number>((c, v) => c.hunger = v), false),
+  buildCharacterUpdateSolver(parseField<number>((c, v) => c.hunger = v), true),
 );
